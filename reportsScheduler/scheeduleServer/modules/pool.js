4085365b7e6ac62ac45e496459b65bf32567fddd
david <david@dpollak44@gmail.com>
2020-07-23 14:29:16 -0400

    update1

--- reportsScheduler/scheeduleServer/modules/pool.js
+++ reportsScheduler/scheeduleServer/modules/pool.js
@@ -1,13 +1,17 @@
 var debug = require('debug')('contacts:pool');
 const mysql = require('mysql');
+require("dotenv").config();
+
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'dpollak44',
-    password: 'password',
-    database: 'pcs'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME
 });
+
 
 pool.on('acquire', function (connection) {
     debug('Connection %d acquired', connection.threadId);
